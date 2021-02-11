fcfb9d4d29bcb6b3230433e31566394a6fba7ebe
David Baumgartner <dv@bmgr.me>
2021-02-11 18:00:14 +0100

    initial commit

---
+++ xmled/lib/xmled.ex
@@ -0,0 +1,18 @@
+defmodule XMLed do
+  @moduledoc """
+  Documentation for `XMLed`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XMLed.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ xmled/test/xm_led_test.exs
@@ -0,0 +1,8 @@
+defmodule XMLedTest do
+  use ExUnit.Case
+  doctest XMLed
+
+  test "greets the world" do
+    assert XMLed.hello() == :world
+  end
+end
